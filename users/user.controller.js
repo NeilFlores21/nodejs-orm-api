2833b9d187e922efb6cafb7729ece200e1e30b36
Neil Jumar Manansala Flores <93628800+NeilFlores21@users.noreply.github.com>
2024-03-12 19:01:44 +0800

    Update user.controller.js

--- users/user.controller.js
+++ users/user.controller.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const router = express. Router();
+const router = express.Router();
 const Joi = require('joi');
 const validateRequest = require('_middleware/validate-request');
 const Role = require('_helpers/role');
